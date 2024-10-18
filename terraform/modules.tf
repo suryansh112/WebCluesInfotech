fd29381a98a10c0058441d1b038314f2e540ae35
Suryansh Srivastava <94421796+suryansh112@users.noreply.github.com>
2024-10-18 14:23:10 +0530

    Update modules.tf

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -5,7 +5,7 @@ module "ec2"{
     key = var.key
     ami = var.ami
     type = var.type
-    ec2_security_group = module.sg_name
+    ec2_security_group = module.sg_group.sg_name
 }
 module "ansible"{
     source = "./ansible"
@@ -14,12 +14,9 @@ module "ansible"{
     key = var.key
     ami = var.ami
     type = var.type
-    ec2_security_group = module.sg_name
+    ec2_security_group = module.sg_group.sg_name
 }
 module "sg_group"{
     source = "./sg"
     name = "web-instance-security-group"
-}
-output sg_name {
-   value = module.sg_group.aws_security_group.ec2.name
 }
